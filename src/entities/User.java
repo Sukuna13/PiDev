8d5c2328f8d2166215a2d9332983e2618a09d12f
ibrahimRguigui <ibrahim.reguigui@esprit.tn>
2022-03-08 01:31:49 +0100

    IbrahimReguigui commit

---
+++ src/entities/User.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Asus
+ */
+public class Salle {
+    private int id ;
+    private String nom;
+
+    public Salle(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Salle{" + "id=" + id + ", nom=" + nom + '}';
+    }
+    
+}
