5ecabddeb31e1d2baf688dd7f3800df4973a4f09
ibrahimRguigui <ibrahim.reguigui@esprit.tn>
2022-03-08 21:35:23 +0100

    IbrahimReguigui push

---
+++ src/entities/Sportif.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Asus
+ */
+public class Sportif {
+    private int id ;
+    private String nom;
+
+    public Sportif(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Sportif{" + "id=" + id + ", nom=" + nom + '}';
+    }
+    
+}
